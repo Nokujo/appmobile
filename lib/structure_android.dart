de733b97dac5147af099286b2d775b3386a886aa
Mustapha <mustaphachouhani2@gmail.com>
2024-11-26 12:18:50 +0100

    deuxième commit

---
+++ lib/structure_android.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Icon _coeur = Icon(Icons.favorite_border, color: Colors.white,);
+  bool _likeBool = false;
+  int _selectedIndex = 0;
+  int currentIndex = 0;
+  String _affichage = 'Accueil';
+  
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+
+
+  void _likeThis(){
+    setState(() {
+      if (_likeBool){
+        _coeur = Icon(Icons.favorite_border, color: Colors.white,);
+        bool _likeBool = false;
+      }
+      else {
+        _coeur = Icon(Icons.favorite, color: Colors.white,);
+        bool _likeBool = true;
+      }
+    });
+  }
+
+  void _itemClique(int index){
+    setState(() {
+      _selectedIndex = index;
+      switch(_selectedIndex){
+        case 0:
+        {
+          _affichage = '$_selectedIndex : Accueil';
+        }
+        break;
+      }
+      switch(_selectedIndex){
+        case 1:
+        {
+          _affichage = '$_selectedIndex : Compte';
+        }
+        break;
+      }
+      switch(_selectedIndex){
+        case 2:
+        {
+          _affichage = '$_selectedIndex :  Enregistrements';
+        }
+        break;
+      }
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(onPressed: _likeThis, icon: _coeur)
+        ],
+        backgroundColor: const Color.fromARGB(255, 58, 136, 21),
+        ),
+       drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu'),
+            ),
+            ListTile(
+              title: const Text('Accueil'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Profil'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Enregistrements'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _likeThis,
+          tooltip: 'Like',
+          child: const Icon(Icons.star),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_affichage),
+            ],
+          ),
+        ),
+
+
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Accueil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Compte',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark),
+              label: 'Enregistrements',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _itemClique,
+        ),
+    );
+  }
+}
+
+
+
